3f0657d762285ddb4cc6a9db80e9fd505e5909ba
romanpryimuk <prijmuk1711@gmail.com>
2023-04-15 13:44:04 +0300

    resetted config for kafka in different way

--- src/main/java/com/example/demo/producer/KafkaProducer.java
+++ src/main/java/com/example/demo/producer/KafkaProducer.java
@@ -1,38 +1,35 @@
 package com.example.demo.producer;
 
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class KafkaProducer {
 
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrap;
+    /**
+     * commented because set up in application.properties
+     **/
 
-    public Map<String, Object> producerConfiguration(){
-        Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrap);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        return props;
-    }
+//    @Value("${spring.kafka.bootstrap-servers}")
+//    private String bootstrap;
+//
+//    public Map<String, Object> producerConfiguration(){
+//        Map<String, Object> props = new HashMap<>();
+//        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrap);
+//        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        return props;
+//    }
+//
+//    @Bean
+//    public ProducerFactory<String, String> producerFactory(){
+//        return new DefaultKafkaProducerFactory<>(producerConfiguration());
+//    }
 
     @Bean
-    public ProducerFactory<String, String> producerFactory(){
-        return new DefaultKafkaProducerFactory<>(producerConfiguration());
-    }
-
-    @Bean
-    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> pf){
+    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> pf) {
         return new KafkaTemplate<>(pf);
     }
 }
